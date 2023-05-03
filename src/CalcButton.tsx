43a83cb3074e1ed22984ebf23335c31cdde3d35f
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-03 18:00:38 +0000

    pasando %

--- src/CalcButton.tsx
+++ src/CalcButton.tsx
@@ -1,6 +1,26 @@
 
 export function CalcButton(props: { setNumber: any; simb: any }){
         const {setNumber, simb}=props
+        const porcentaje = (num1*number)/100
+            const strNum1 = num1.toString()
+            const strNumber = number.toString()
+            if (operation === "+") {
+              console.log(num, index);
+              setNumber(num1 + porcentaje)
+            } else if (operation === "-") {
+              console.log(num, index);
+              setNumber(num1 - porcentaje)
+            } else if (operation === "*") {
+              console.log(num, index);
+              setNumber(num1 * porcentaje)
+            } else if (operation === "/") {
+              console.log(num, index);
+              setNumber(num1 / porcentaje)
+            } else {
+              console.log("No hay operacion")
+            }
+            setPrevNumber(strNum1 + operation + strNumber + "%=")
+            num1Arr = []
     return <button className="buttons">
         {simb}
     </button>
